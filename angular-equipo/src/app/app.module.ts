0c6b9c4cfdfbac3faaed2b57f30cf31256bf6b64
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2022-03-25 22:36:03 -0600

    creasion y eliminacion
    elimine unos componentes creados por error en tablas.
    agregue los componentes usuarios y productos estos con tendrán las tablas y formularios para hacer un CRUD.

--- angular-equipo/src/app/app.module.ts
+++ angular-equipo/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login/login.component';
 import { InicioComponent } from './main/inicio/inicio.component';
+import { TablasComponent } from './main/tablas/tablas.component';
+import { UsuariosComponent } from './main/tablas/usuarios/usuarios.component';
+import { ProductosComponent } from './main/tablas/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    TablasComponent,
+    UsuariosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
